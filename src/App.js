518f7aa6a69cf94f48ae5cd2a729c59a2b6d796f
Rahi999 <101566073+Rahi999@users.noreply.github.com>
2022-05-18 09:05:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>LOGOBAKERY</div>
+      <div>Services</div>
+      <div>Projects</div>
+      <div>About</div>
+      <button>Contact</button>
+    </div>
+  );
+}
